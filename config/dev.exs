5ee1657fd06914d65cc8c4372fab5fe71061fa0f
Alexander Sysoev <alexsblsss@gmail.com>
2019-04-22 09:28:59 -0400

    Remove real data from config

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :meli_api_client,
-  app_id: "2996228106246832",
-  app_secret: "vdWT4LjAwxtAJxLUmdZ7H8DqAGqfjvVp"
+  app_id: System.get_env("MELI_APP_ID"),
+  app_secret: System.get_env("MELI_APP_SECRET")
